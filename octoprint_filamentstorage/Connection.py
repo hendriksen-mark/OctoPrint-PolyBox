eb1d3308330802c0051d741ca818d78ad22e9946
Mark <hendriksen-mark@hotmail.com>
2022-02-27 01:44:40 +0100

    Update Connection.py

--- octoprint_filamentstorage/Connection.py
+++ octoprint_filamentstorage/Connection.py
@@ -6,6 +6,8 @@ import threading
 import serial
 import serial.tools.list_ports
 
+__plugin_pythoncompat__ = ">=2.7,<4"
+
 HUMIDITY_EXPRESSION = "^H:+([\\-0-9]+[.]*[0-9]*)%.*"
 L1_EXPRESSION = ".*L1:+([\\-0-9]+[.]*[0-9]*)mm.*"
 L2_EXPRESSION = ".*L2:+([\\-0-9]+[.]*[0-9]*)mm.*"
