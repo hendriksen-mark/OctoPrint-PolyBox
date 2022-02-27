b0e26d2b2fe3a5cd2a53af8a791b3217fb6c6d58
Mark <hendriksen-mark@hotmail.com>
2022-02-27 02:01:18 +0100

    Update __init__.py

--- octoprint_filamentstorage/__init__.py
+++ octoprint_filamentstorage/__init__.py
@@ -125,6 +125,7 @@ class FilamentstoragePlugin(octoprint.plugin.StartupPlugin,
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Filament Storage"
+__plugin_pythoncompat__ = ">2.7,<4"
 
 
 def __plugin_load__():
